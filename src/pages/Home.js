bccb54e21d0c5745e87c9e5ebc7df9af00a9e7a7
Sean <Spglancy@gmail.com>
2019-03-04 13:40:29 -0800

    fixed images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -30,12 +30,12 @@ function Home() {
       </div>
       <section className="interstitial examples" />
       <section className="white" id="services">
-        <div className="copy is-hidden"><img alt="placeholder" src="http://i68.tinypic.com/fazct3.png" height={600} /></div>
+        <div className="copy is-hidden"><img alt="placeholder" src={require("../assets/phoneScreens.png")} height={600} /></div>
         <div className="log" />
         <div className="title">Who It's For</div>
         <h2>All You Need Is An Apple Watch And We Do The Rest</h2>
         <p>Download the app and grant RecoMinder permissions to all data in HealthKit and notification access. From there, RecoMinder will start giving you the reminders and Recomendations that you need.</p>
-        <div className="watch"><img alt="placeholder" src="https://cdn1.imggmi.com/uploads/2019/1/30/84f539f29d528cd513e54d6b16c33572-full.png" height={500} /></div>
+        <div className="watch"><img alt="placeholder" src={require("../assets/watchFace.png")} height={500} /></div>
         <div className="title">
           <br />
           How Does It Work?
@@ -44,7 +44,7 @@ function Home() {
         <p>We all know that your heart rate can provide insights into your biological wellbeing. At RecoMinder we take these insights and compare them to your Apple Watch HealthKit data to give our best Recommendations for things you need in the form of a timely notification.</p>
         <div className="flex-container thin-width">
           <div className="column">
-            <div className="icons"><img alt="placeholder" src="https://static.thenounproject.com/png/85887-200.png" height={50} /></div>
+            <div className="icons"><img alt="placeholder" src={require("../assets/waterDroplet.png")} height={50} /></div>
             <h3>Water Intake</h3>
             <p>RecoMinder sends you daily notifications on the best time to drink water based upon your heart rate and how long it's been since your last intake. You can also set a daily water intake goal that RecoMinder can help you achieve.</p>
             <a href="/serieskit" className="inline-cta">
@@ -55,7 +55,7 @@ function Home() {
             </a>
           </div>
           <div className="column">
-            <div className="icons"><img alt="placeholder" src="https://cdn1.imggmi.com/uploads/2019/1/30/b9d4bd52f0b562cac47bba17be340d46-full.png" height={50} /></div>
+            <div className="icons"><img alt="placeholder" src={require("../assets/runningman.png")} height={50} /></div>
             <h3>Exercise Time</h3>
             <p>It's hard to find the perfect time to exercise outdoors when so many factors come into play. RecoMinder simplifies this decision for you by taking into account weather conditions, the UV index, and your heart rate to notify you when it's the right time.</p>
             <a href="/sermonclips" className="inline-cta">
